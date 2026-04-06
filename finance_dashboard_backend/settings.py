807effc0dde05130b1bb94f3bfaef6326fc83671
Nayana Dev <devnayana03@gmail.com>
2026-04-06 17:50:43 +0530

    Fix ALLOWED_HOSTS for Render deployment

--- finance_dashboard_backend/settings.py
+++ finance_dashboard_backend/settings.py
@@ -27,10 +27,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-import os
-
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
-
+ALLOWED_HOSTS = [
+    "finance_dashboard_backend.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 # Application definition
 
