c92f4f57eaa32eddc166ab3f5881e8107bb8377e
Nayana Dev <devnayana03@gmail.com>
2026-04-06 19:12:55 +0530

    Add superuser

--- finance_dashboard_backend/settings.py
+++ finance_dashboard_backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,9 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import os
-
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = "=)v_f$@zqv0x94wtybg@0*_9uy&3fh)+p41@dh*%)6q^9zk0l1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
